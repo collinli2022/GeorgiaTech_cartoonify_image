ffa852d3264ea95e342fc258ea78cc9219939417
Collin <collinli2022@gmail.com>
2021-06-25 13:53:41 -0400

    added cartoonify feature

--- app.py
+++ app.py
@@ -6,10 +6,68 @@ import io
 import os
 from werkzeug.utils import secure_filename
 
+import cv2 # for image processing
+import numpy as np # to store image
+
+
+# input a image path and it will output a cartoonified image
+def better_cartoonify(numpy_arr_image, numDownSamples = 2, numBilateralFilters = 15, resize_shape=(1920,1080)):
+    ## Get image ##
+    orig = np.array(numpy_arr_image) 
+    #orig = cv2.cvtColor(orig, cv2.COLOR_BGR2RGB)
+    images = {}
+
+    ## Original ##
+    ## Resize to resize_shape ##
+    images['orig1'] = cv2.resize(orig, resize_shape)
+
+    ## Placeholder ##
+    images['placeholder'] = images['orig1'].copy()
+
+    ## downsample image using Gaussian pyramid ##
+    for _ in range(numDownSamples): 
+      orig = cv2.pyrDown(orig)
+    images['downsample'] = cv2.resize(orig, resize_shape)
+
+    ## repeatedly apply small bilateral filter instead of applying ##
+    ## one large filter ##
+    for _ in range(numBilateralFilters): 
+      orig = cv2.bilateralFilter(orig, 2, 2, 2) # arguments of diameter of each pixel neighborhood, sigmaColor, sigmaColor (https://www.geeksforgeeks.org/python-bilateral-filtering/)
+    images['bilateral'] = cv2.resize(orig, resize_shape)
+
+    # upsample image to original size 
+    for _ in range(numDownSamples): 
+      orig = cv2.pyrUp(orig)
+    images['upsample'] = cv2.resize(orig, resize_shape)
+
+    ## MedianBlur for even more blur ##
+    # orig = cv2.medianBlur(orig, 3)
+    images['blur'] = cv2.resize(orig, resize_shape)
+
+    ## Grayscale (to improve smoothing) ##
+    grayScaleImage = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+    images['grayscale'] = cv2.resize(grayScaleImage, resize_shape)
+
+    ## Adaptive Edge Threshold ## # TODO: thinner edges and greater threshold 
+    getEdge = cv2.adaptiveThreshold(grayScaleImage, 255, 
+                                    cv2.ADAPTIVE_THRESH_MEAN_C, 
+                                    cv2.THRESH_BINARY, 9, 2) # TODO: increase threshold
+    images['edge'] = cv2.resize(getEdge, resize_shape)
+
+    ## Color Filter ##
+    colorImage = cv2.bilateralFilter(orig, 9, 300, 300) # filter color
+    images['color filter'] = cv2.resize(colorImage, resize_shape)
+
+    ## Combined ##
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge) # combind image and edges
+    images['combined'] = cv2.resize(cartoonImage, resize_shape)
+
+    im = Image.fromarray(cartoonImage)
+    return im
+
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-
 
 ## Reference: https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/ ##
 def allowed_file(filename):
@@ -33,8 +91,9 @@ def upload_file():
             filename = secure_filename(file.filename)
             print(type(file))
             inputed_image = Image.open(file)
+            adjusted_image = better_cartoonify(inputed_image.convert('RGB'))
             data = io.BytesIO()
-            inputed_image.save(data, "JPEG")
+            adjusted_image.save(data, "JPEG")
             encoded_img_data = base64.b64encode(data.getvalue())
             # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return render_template("index.html", img_data=encoded_img_data.decode('utf-8'))
